2d6cbadc6fc0dcb4d8611791458dae1271ef821b
RodriGonca <rodrigo.goncalves@me.com>
2019-07-25 14:19:31 -0300

    Created a dedicated function for DataPrep to be used across the models.

--- scripts/step_01_create_functions.R
+++ scripts/step_01_create_functions.R
@@ -47,3 +47,88 @@ GetAgeFromBirthnumber <- function(var_birth_number) {
   
   return(result)
 }
+
+# DataPrep --------------------------------------------------------------------
+# The objective of this function is to return a DataFrame to be used in predictive modeling.
+# this function will join loan, client, district, creditcard, account_balance, account_balance_pattern
+# rename the variables and create the appropriate dummy variables to be used in the modeling process.
+
+DataPrep <- function() {
+
+  temp <- left_join(loan, disposition, by = c('account_id', 'type')) %>% 
+    left_join(client, by = 'client_id') %>%
+    left_join(district, by = 'district_id') %>% 
+    left_join(creditcard, by = 'disp_id') %>% 
+    left_join(account_balance, by = 'account_id') %>% 
+    left_join(account_transaction_pattern, by = 'account_id') %>% 
+    mutate(card_age_month = (issued %--% 
+                               make_date(1998, 12, 31)) / months(1),
+           last_transaction_age_days = ((last_transaction_date.y %--% 
+                                           make_date(1998, 12, 31)) / days(1))) %>% 
+    dplyr::select(c("amount.x", "duration", "payments", "status", "defaulter", 
+                    "contract_status", "gender", "age", "district_name", 
+                    "region", "no_of_inhabitants", 
+                    "no_of_municip_inhabitants_less_499", 
+                    "no_of_municip_500_to_1999", "no_of_municip_2000_to_9999", 
+                    "no_of_municip_greater_10000", "no_of_cities", 
+                    "ratio_of_urban_inhabitants", 
+                    "average_salary", "unemploymant_rate_1995", 
+                    "unemploymant_rate_1996", 
+                    "no_of_enterpreneurs_per_1000_inhabitants", 
+                    "no_of_commited_crimes_1995", 
+                    "no_of_commited_crimes_1996", "type.y", 
+                    "card_age_month","account_balance", 
+                    "avg_balance","transaction_count", "amount.y", 
+                    "last_transaction_age_days", "prop_old_age_pension", 
+                    "prop_insurance_payment", 
+                    "prop_sanction_interest","prop_household", 
+                    "prop_statement", "prop_interest_credited", 
+                    "prop_loan_payment", "prop_other"))
+  
+  colnames(temp) <- c("x_loan_amount", "x_loan_duration", "x_loan_payments", 
+                      "x_loan_status", "y_loan_defaulter", "x_loan_contract_status",
+                      "x_client_gender", "x_client_age", 
+                      "x_district_name", "x_region", 
+                      "x_no_of_inhabitants", "x_no_of_municip_inhabitants_less_499", 
+                      "x_no_of_municip_500_to_1999", "x_no_of_municip_2000_to_9999", 
+                      "x_no_of_municip_greater_10000", "x_no_of_cities", 
+                      "x_ratio_of_urban_inhabitants", 
+                      "x_average_salary", "x_unemploymant_rate_1995", 
+                      "x_unemploymant_rate_1996", 
+                      "x_no_of_enterpreneurs_per_1000_inhabitants", 
+                      "x_no_of_commited_crimes_1995", 
+                      "x_no_of_commited_crimes_1996", "x_card_type", 
+                      "x_card_age_month","x_account_balance", 
+                      "x_avg_account_balance","x_transaction_count", 
+                      "x_transaction_amount", "x_last_transaction_age_days", 
+                      "x_prop_old_age_pension", "x_prop_insurance_payment", 
+                      "x_prop_sanction_interest","x_prop_household","x_prop_statement",
+                      "x_prop_interest_credited", "x_prop_loan_payment", "x_prop_other")
+  
+  
+  
+  temp$x_card_type = ifelse(is.na(temp$x_card_type), 'no card', 
+                            as.character(temp$x_card_type))
+  
+  temp$x_card_age_month = ifelse(is.na(temp$x_card_age_month), 0, 
+                                 temp$x_card_age_month)
+  
+  temp$y_loan_defaulter = as.numeric(temp$y_loan_defaulter)
+  
+  temp <- dummy_cols(temp, 
+                     remove_first_dummy = TRUE,
+                     select_columns = c("x_loan_status", "x_loan_contract_status",
+                                        "x_client_gender", "x_district_name", "x_region", 
+                                        "x_card_type"))
+  
+  temp <- dplyr::select(temp, -c("x_loan_status", "x_loan_contract_status",
+                          "x_client_gender", "x_district_name", "x_region", 
+                          "x_card_type"))
+  
+  temp <- temp[ , order(names(temp))]
+  
+  temp <- dplyr::select(temp, y_loan_defaulter, everything())
+
+  return(temp)
+
+}
