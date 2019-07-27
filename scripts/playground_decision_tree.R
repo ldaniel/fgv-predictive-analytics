db16730f1ad39f926a781a779ea97162596bef70
RodriGonca <rodrigo.goncalves@me.com>
2019-07-27 18:53:59 -0300

    renaming variable names to avoid conflict with other models.

--- scripts/playground_decision_tree.R
+++ scripts/playground_decision_tree.R
@@ -1,22 +1,22 @@
 # data prep -------------------------------------------------------------------
 
-temp <- DataPrep()
-temp <- dplyr::select(temp, -starts_with('x_district'), 
+loan_dataset_DT <- DataPrep()
+loan_dataset_DT <- dplyr::select(loan_dataset_DT, 
                       -x_prop_old_age_pension)
 
-kable(tibble(variables = names(temp)))
+kable(tibble(variables = names(loan_dataset_DT)))
 
 # sampling ----------------------------------------------------------------------------
 
 set.seed(12345)
-index <- caret::createDataPartition(loan_dataset$y_loan_defaulter, 
+index <- caret::createDataPartition(loan_dataset_DT$y_loan_defaulter, 
                                     p= 0.7,list = FALSE)
-data.train <- loan_dataset[index, ]
-data.test  <- loan_dataset[-index,]
+data.train_DT <- loan_dataset_DT[index, ]
+data.test_DT  <- loan_dataset_DT[-index,]
 
-event_proportion <- bind_rows(prop.table(table(loan_dataset$y_loan_defaulter)),
-                              prop.table(table(data.train$y_loan_defaulter)),
-                              prop.table(table(data.test$y_loan_defaulter)))
+event_proportion <- bind_rows(prop.table(table(loan_dataset_DT$y_loan_defaulter)),
+                              prop.table(table(data.train_DT$y_loan_defaulter)),
+                              prop.table(table(data.test_DT$y_loan_defaulter)))
 
 event_proportion$scope = ''
 event_proportion$scope[1] = 'full dataset'
@@ -29,8 +29,8 @@ kable(event_proportion)
 
 # fit the decision tree model -------------------------------------------------------------
 
-tree.full <- rpart(data= data.train, y_loan_defaulter ~ .,
-                   control = rpart.control(minbucket=20),
+tree.full <- rpart(data= data.train_DT, y_loan_defaulter ~ .,
+                   control = rpart.control(minbucket=10),
                    method = "class")
 
 tree.full
@@ -41,6 +41,7 @@ printcp(tree.full)
 plotcp(tree.full)
 
 tree.prune <- prune(tree.full, cp= tree.full$cptable[which.min(tree.full$cptable[,"xerror"]),"CP"])
+tree.prune <- tree.full
 
 plotcp(tree.prune)
 rpart.plot(tree.prune)
