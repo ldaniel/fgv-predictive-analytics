ea17ce3616fd683a020fb77ccbb85c76ef86dc18
RodriGonca <rodrigo.goncalves@me.com>
2019-08-01 13:27:12 -0300

    removing redundant variable

--- scripts/step_00_config_environment.R
+++ scripts/step_00_config_environment.R
@@ -8,7 +8,6 @@ options(encoding = "UTF-8")
 username <- Sys.info()[["user"]]
 directoryPath <- dirname(rstudioapi::getSourceEditorContext()$path)
 directoryPath <- str_replace(directoryPath, "/scripts", "")
-wd = directoryPath
 
-setwd(wd)
+setwd(directoryPath)
 getwd()
